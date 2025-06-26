876fb2f13bfabde7ef6aba65a5052dee09bbd861
José <jir@galoy.io>
2025-06-26 15:25:43 -0600

    feat(reporting): serialize reports differently depending on norm requirements (#2284)
    * chore(reporting): organize reports by norm in bucket
    
    As the number of reports increases, it will be good to keep them as organized as possible,
    this is a simple first stab at it.  Also provides a simple way to
    use a different serialization format depending on the norm.
    
    * feat(reporting): serialize El Salvador reports differently depending on norm
    
    Some norms require xml, some require pipe delimited text files.
    This PR implements these requirements for the normas we're generating.
    
    * fix: include nrp_51 reports

--- meltano/utilities/generate-es-reports/generate_es_reports/__init__.py
+++ meltano/utilities/generate-es-reports/generate_es_reports/__init__.py
@@ -1,8 +1,14 @@
 import os
+import io
+import csv
 from datetime import datetime
 from google.cloud import bigquery, storage
 from dicttoxml import dicttoxml
 from google.oauth2 import service_account
+from re import compile
+
+table_name_pattern = compile(r"report_([0-9a-z_]+)_\d+_(.+)")
+
 
 def main():
     required_envs = [
@@ -25,30 +31,52 @@ def main():
             "GOOGLE_APPLICATION_CREDENTIALS environment variable must be set to the path of a valid service account JSON file."
         )
     credentials = service_account.Credentials.from_service_account_file(keyfile)
-
     bq_client = bigquery.Client(project=project_id, credentials=credentials)
 
-    # List all tables in the dataset and process those starting with "report_"
     tables_iter = bq_client.list_tables(dataset)
     for table in tables_iter:
-        if not table.table_id.startswith("report_"):
+        table_name = table.table_id
+        match = table_name_pattern.match(table_name)
+        if not match:
             continue
-        report_name = table.table_id
-        query = f"SELECT * FROM `{project_id}.{dataset}.{report_name}`;"
+        norm_name = match.group(1)
+        report_name = match.group(2)
+        date_str = datetime.now().strftime("%Y-%m-%d")
+
+        query = f"SELECT * FROM `{project_id}.{dataset}.{table_name}`;"
         query_job = bq_client.query(query)
         rows = query_job.result()
         field_names = [field.name for field in rows.schema]
         rows_data = [{name: row[name] for name in field_names} for row in rows]
-        # Remove item_root/item_wrap, use only custom_root and attr_type
-        xml_bytes = dicttoxml(rows_data, custom_root="rows", attr_type=False)
-        xml_content = xml_bytes.decode("utf-8")
-        date_str = datetime.now().strftime("%Y-%m-%d")
-        blob_path = f"reports/{date_str}/{report_name}.xml"
+
+        if norm_name in ["nrp_41", "nrp_51"]:
+            report_content_type = "text/xml"
+            report_bytes = dicttoxml(rows_data, custom_root="rows", attr_type=False)
+            report_content = report_bytes.decode("utf-8")
+            blob_path = f"reports/{date_str}/{norm_name}/{report_name}.xml"
+
+        elif norm_name == "nrsf_03":
+            report_content_type = "text/plain"
+            output = io.StringIO()
+            writer = csv.DictWriter(
+                output, fieldnames=field_names, delimiter="|", lineterminator="\n"
+            )
+            writer.writeheader()
+            writer.writerows(rows_data)
+            report_content = output.getvalue()
+            blob_path = f"reports/{date_str}/{norm_name}/{report_name}.txt"
+
+        else:
+            # TODO: we'll have to come up with some kind of convention for
+            # serializing ad-hoc internal reports when we have those
+            pass
+
         storage_client = storage.Client(project=project_id, credentials=credentials)
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(blob_path)
-        blob.upload_from_string(xml_content, content_type="text/xml")
+        blob.upload_from_string(report_content, content_type=report_content_type)
         print(f"Uploaded XML report to gs://{bucket_name}/{blob_path}")
+
 
 if __name__ == "__main__":
     main()
