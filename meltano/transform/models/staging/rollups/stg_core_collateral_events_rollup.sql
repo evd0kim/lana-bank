e7c5e7e756f626375a1c53845d971b142d12e88e
sebastienverreault <15014256+sebastienverreault@users.noreply.github.com>
2025-06-26 11:29:38 +0900

    feat: intermediate rollups (#2280)
    * update: source with new rollup tables
    
    * fix: id is kept as is in stg table
    
    * feat: new sources
    
    * feat: new rollups

---
+++ meltano/transform/models/staging/rollups/stg_core_collateral_events_rollup.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = ['id'],
+) }}
+
+with ordered as (
+    select
+        *,
+        row_number()
+            over (
+                partition by id
+                order by _sdc_received_at desc
+            )
+            as order_received_desc
+
+    from {{ source("lana", "public_core_collateral_events_rollup_view") }}
+
+    {% if is_incremental() %}
+        where
+            _sdc_batched_at >= (select coalesce(max(_sdc_batched_at), '1900-01-01') from {{ this }})
+    {% endif %}
+)
+
+select
+    * except (order_received_desc),
+from ordered
+where order_received_desc = 1
---
+++ meltano/transform/models/staging/rollups/stg_core_interest_accrual_cycle_events_rollup.sql
@@ -0,0 +1,28 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = ['id'],
+) }}
+
+with ordered as (
+    select
+        *,
+        row_number()
+            over (
+                partition by id
+                order by _sdc_received_at desc
+            )
+            as order_received_desc
+
+    from {{ source("lana", "public_core_interest_accrual_cycle_events_rollup_view") }}
+
+    {% if is_incremental() %}
+        where
+            _sdc_batched_at >= (select coalesce(max(_sdc_batched_at), '1900-01-01') from {{ this }})
+    {% endif %}
+)
+
+select
+    facility_id as credit_facility_id,
+    * except (facility_id, order_received_desc),
+from ordered
+where order_received_desc = 1
